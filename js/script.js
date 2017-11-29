007483e52d82d1122dbe58767ec181a8bcb4b949
Piyush Kumar <kpiyush1709@gmail.com>
2017-11-29 22:57:50 +0530

    fetch metadata of input urls

--- js/script.js
+++ js/script.js
@@ -6,8 +6,10 @@ const overlay = document.querySelector('.overlay');
 const bookmarkForm = document.querySelector('.bookmark-form');
 const bookmarkInput = bookmarkForm.querySelector("input[type=text]");
 const bookmarkList = document.querySelector('.bookmarks-list');
-
 const bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+
+const apiUrl        = 'https://opengraph.io/api/1.0/site';
+const appId         = '';                                       // Add your own api key here. 
 
 fillBookmarkList(bookmarks);
 
@@ -23,17 +25,29 @@ function hideFloater(){
 function addBookmark(event){
     event.preventDefault();
 
-    const title = bookmarkInput.value;
+    if(!bookmarkInput.value) return;
 
-    const bookmark = {
-        title: title
-    };
+    const url = encodeURIComponent(bookmarkInput.value);
 
-    bookmarks.push(bookmark);
-    fillBookmarkList(bookmarks);
-    storeBookmark(bookmarks);
-    bookmarkForm.reset();
-    
+    fetch(`${apiUrl}/${url}?app_id=${appId}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const bookmark = {
+                title: data.hybridGraph.title,
+                image: data.hybridGraph.image,
+                link: data.hybridGraph.url
+            };
+        
+            bookmarks.push(bookmark);
+            fillBookmarkList(bookmarks);
+            storeBookmark(bookmarks);
+            bookmarkForm.reset();
+        })
+        .catch(error => {
+            console.log("Problem in fetching url data");
+        });
+
     // const title = bookmarkInput.value;
     // const bookmark = document.createElement('a');
     // bookmark.href = "#";
@@ -47,8 +61,8 @@ function addBookmark(event){
 function fillBookmarkList(bookmarks = []){
 
     const bookmarksHtml = bookmarks.map((bookmark,i) => {
-        return `<a href="#" class="bookmark" data-id="${i}">
-            <div class="img"></div>
+        return `<a href="${bookmark.link}" class="bookmark" data-id="${i}" target="_blank">
+            <div class="img" style="background-image:url('${bookmark.image}')"></div>
             <div class="title">${bookmark.title}</div>
             <span class="glyphicon glyphicon-remove"></span>
         </a>
@@ -70,7 +84,6 @@ function deleteBookmark(e){
     bookmarks.splice(index, 1);
     fillBookmarkList(bookmarks);
     storeBookmark(bookmarks);
-
 }
 
 input.addEventListener('focus', showFloater);
